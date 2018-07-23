367089ebc8d7d1f0d1c71232e2ba2672babfffc1
Ivaylo Asenov <ivaylo.asenov@hotmail.com>
2018-07-23 10:03:05 -0400

    using KerasClassifier

--- K-Titanic/python.py
+++ K-Titanic/python.py
@@ -17,7 +17,7 @@ X = pd.get_dummies(X, columns=['Pclass', 'Sex', 'Embarked'], drop_first = True)
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
@@ -33,15 +33,46 @@ from keras.layers import Dense, Dropout
 model = Sequential()
 
 model.add(Dense(6, kernel_initializer='uniform', activation='relu', input_dim=9))
+model.add(Dropout(0.2))
 model.add(Dense(6, kernel_initializer='uniform', activation='relu'))
 model.add(Dense(1, kernel_initializer='uniform', activation='sigmoid'))
-
+model.add(Dropout(0.1))
 model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
 model.fit(X_train, y_train, batch_size = 10, epochs = 100)
 
+# Tuning the ANN
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import GridSearchCV
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+
+def build_model(optimizer, n_layer_one, n_layer_two):
+    model = Sequential()
+    model.add(Dense(n_layer_one, kernel_initializer='uniform', activation='relu', input_dim=9))
+    model.add(Dropout(0.2))
+    model.add(Dense(n_layer_two, kernel_initializer='uniform', activation='relu'))
+    model.add(Dropout(0.1))
+    model.add(Dense(1, kernel_initializer='uniform', activation='sigmoid'))
+    model.compile(optimizer = optimizer, loss = 'binary_crossentropy', metrics = ['accuracy'])
+    return model
+
+model = KerasClassifier(build_fn=build_model)
+parameters = {'batch_size' : [12, 24],
+              'epochs'   : [100, 300],
+              'optimizer'  : ['adam', 'rmsprop'],
+              'n_layer_one': [6, 12],
+              'n_layer_two': [6, 12]}
+
+grid_search = GridSearchCV(estimator = model, param_grid = parameters, scoring = 'accuracy', cv = 10)
+grid_search = grid_search.fit(X_train, y_train)
+
+best_accuracy = grid_search.best_params_
+
 y_pred = model.predict(X_test)
 y_pred = (y_pred > 0.5)
+
+
 
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
