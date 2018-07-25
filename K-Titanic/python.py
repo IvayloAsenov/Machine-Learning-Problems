d3715338daed10b0e6d47f1297213473bd1af98c
Ivaylo Asenov <ivaylo.asenov@hotmail.com>
2018-07-25 12:16:07 -0400

    new dataset/92% accuracy on validation set

--- K-Titanic/python.py
+++ K-Titanic/python.py
@@ -8,12 +8,18 @@ test_dataset  = pd.read_csv('test.csv')
 
 # Fill in missing values
 train_dataset['Age'].fillna(train_dataset['Age'].mean(), inplace=True)
+train_dataset['Fare'].fillna(train_dataset['Fare'].mean(), inplace=True)
+test_dataset['Age'].fillna(test_dataset['Age'].mean(), inplace=True)
+test_dataset['Fare'].fillna(test_dataset['Fare'].mean(), inplace=True)
 
 X = train_dataset.iloc[:, [2, 4, 5, 6, 7, 9, 11]]
 y = train_dataset.iloc[:, 1]
 
+X_test_data = test_dataset.iloc[:, [1, 3, 4, 5, 6, 8, 10]]
+
 # Encoding categorical data
 X = pd.get_dummies(X, columns=['Pclass', 'Sex', 'Embarked'], drop_first = True)
+X_test_data = pd.get_dummies(X_test_data, columns=['Pclass', 'Sex', 'Embarked'], drop_first = True)
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
@@ -24,6 +30,7 @@ from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
+X_test_data = sc.transform(X_test_data)
 
 # Making the ANN
 import keras
@@ -33,13 +40,30 @@ from keras.layers import Dense, Dropout
 model = Sequential()
 
 model.add(Dense(6, kernel_initializer='uniform', activation='relu', input_dim=9))
-model.add(Dropout(0.2))
-model.add(Dense(6, kernel_initializer='uniform', activation='relu'))
 model.add(Dense(1, kernel_initializer='uniform', activation='sigmoid'))
-model.add(Dropout(0.1))
 model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
-model.fit(X_train, y_train, batch_size = 10, epochs = 100)
+model.fit(X_train, y_train, batch_size = 24, epochs = 120)
+y_pred = model.predict(X_test_data)
+y_pred = (y_pred > 0.5)
+
+# K-Fold validation ANN 
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+
+def build_model():
+    model = Sequential()
+    model.add(Dense(6, kernel_initializer='uniform', activation='relu', input_dim=9))
+    model.add(Dense(1, kernel_initializer='uniform', activation='sigmoid'))
+    model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+    return model
+
+classifier = KerasClassifier(build_fn=build_model, batch_size = 24, epochs = 120)
+accuracies = cross_val_score(estimator=classifier, X = X_train, y = y_train, cv = 10)
+mean = accuracies.mean()
+variance = accuracies.std()
 
 # Tuning the ANN
 from keras.wrappers.scikit_learn import KerasClassifier
@@ -47,7 +71,7 @@ from sklearn.model_selection import GridSearchCV
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
 
-def build_model(optimizer, n_layer_one, n_layer_two):
+def build_model(optimizer):
     model = Sequential()
     model.add(Dense(6, kernel_initializer='uniform', activation='relu', input_dim=9))
     model.add(Dropout(0.2))
@@ -65,11 +89,12 @@ parameters = {'batch_size' : [12, 24],
 grid_search = GridSearchCV(estimator = model, param_grid = parameters, scoring = 'accuracy', cv = 5)
 grid_search = grid_search.fit(X_train, y_train)
 
-best_accuracy = grid_search.best_params_
+best_parameters = grid_search.best_params_
+best_score = grid_search.best_score_
 
 y_pred = model.predict(X_test)
 y_pred = (y_pred > 0.5)
-
+y_pred = y_pred * 1
 
 
 # Making the Confusion Matrix
--- simpsonsCNN/python.py
+++ simpsonsCNN/python.py
@@ -1,114 +1,49 @@
-import numpy as np
-np.set_printoptions(precision=2)
-
-import cv2, glob
-import pandas as pd
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers import Convolution2D, MaxPooling2D
-from keras.utils import np_utils, to_categorical
-from keras.optimizers import Adagrad
+from keras.layers import Convolution2D, MaxPooling2D, Flatten, Dense, Dropout
 
-from sklearn.metrics import confusion_matrix, classification_report
-from sklearn.model_selection import train_test_split
+# Build the CNN
 
-import pylab
+classifier = Sequential()
 
-import itertools
+classifier.add(Convolution2D(32, 3, 3, input_shape = (64, 64, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
 
-from matplotlib import pyplot as plt
+classifier.add(Flatten())
 
-np.random.seed(42)  # for reproducibility
+classifier.add(Dense(output_dim = 128, activation = 'relu'))
+classifier.add(Dropout(0.5))
 
-df = pd.read_csv('classes.txt'); # read the classes
-df.head(); # go at the head
+classifier.add(Dense(output_dim = 7, activation = 'softmax'))
 
-image_size = 128 # images will be resized to 128 * 128 (RGB)
-labels_name = df.label.unique() # take only the unique labels (create a mapping)
+classifier.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
 
-def main():
+from keras.preprocessing.image import ImageDataGenerator
 
-    pics = []
-    labels = []
+train_datagen = ImageDataGenerator(rescale=1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
 
-    for index, row in df.iterrows(): # go row by row
-        filepath = './dataset/' + row["filename"] # find filepath
-        temp = cv2.imread(filepath) # read the image
+test_datagen = ImageDataGenerator(rescale = 1./255)
 
-        if temp is not None:
-            temp = cv2.resize(temp, (image_size, image_size)) # resize the image to 128 * 128
-            pics.append(temp) # add to pics
-            labels.append(np.where(labels_name == row["label"])) # add the correct label
+training_set = train_datagen.flow_from_directory('data/training',
+                                                 target_size = (64, 64),
+                                                 batch_size = 32,
+                                                 class_mode = 'categorical')
 
-    input_shape = (image_size, image_size, 3) # input is 128 * 128 * 3
+test_set = train_datagen.flow_from_directory('data/test',
+                                             target_size = (64, 64),
+                                             batch_size = 32,
+                                             class_mode = 'categorical')
 
-    X = np.array(pics) # those are out inputs
-    y = np.array(labels) # those are out outputs
+classifier.fit_generator(training_set,
+                         epochs = 25,
+                         validation_data = test_set)
 
-    y = to_categorical(y, len(labels_name)).reshape((len(y), 7)) # Converts a class vector (integers) to binary class matrix.
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15, stratify=y,random_state=42); # split the dataset
 
-    # display some samples
-    # for idx in np.random.choice(len(X_train), 5):
-    #     show_example(idx, X_train, y_train, [])
-
-    # normalize
-    X_train_n = X_train.astype('float32')
-    X_test_n = X_test.astype('float32')
-    X_train_n /= 255
-    X_test_n /= 255
-
-    #build our CNN
-    model = Sequential()
-    model.add(Convolution2D(128, kernel_size=(6, 6), padding='same', strides=(4, 4), activation='relu', input_shape=input_shape))
-    model.add(Convolution2D(32, kernel_size=(3, 3), padding='same', strides=(2, 2), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-    model.add(Convolution2D(12, kernel_size=(2, 2), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Flatten())
-    model.add(Dropout(0.5))
-    model.add(Dense(42, activation='relu'))
-    model.add(Dense(7, activation='softmax'))
-
-    # model summary
-    # print(model.summary())
-
-    # train the model
-    model.compile(optimizer=Adagrad(), loss='categorical_crossentropy', metrics=['accuracy'])
-    history = model.fit(X_train_n, y_train, validation_split=0.07, epochs=200, batch_size=8)
-
-    history_dict = history.history
-    loss_values = history_dict['loss']
-    val_loss_values = history_dict['val_loss']
-    epochs = range(1, len(loss_values) + 1)
-
-    plt.plot(epochs, loss_values, 'b')
-    plt.plot(epochs, val_loss_values, 'g')
-    plt.xlabel('Epochs')
-    plt.ylabel('Loss')
-
-    pylab.show()
-
-    predictions = model.predict(X_test_n, verbose=0)
-
-    np.argmax(predictions, axis=1)
-    np.argmax(y_test, axis=1)
-    accuracy = np.sum(np.argmax(predictions, axis=1) == np.argmax(y_test, axis=1)) / predictions.shape[0]
-
-    # Let's identify the good predictions and the bad ones
-    correct = [idx for idx, correct in enumerate(np.argmax(predictions, axis=1).astype(bool) == np.argmax(y_test, axis=1).astype(bool)) if correct]
-    misclassification = [idx for idx, incorrect in enumerate(np.argmax(predictions, axis=1) != np.argmax(y_test, axis=1)) if incorrect]
-
-    for idx in np.random.choice(correct, 5):
-        show_example(idx, X_test, y_test, predictions)
-
-    print(len(correct))
-    print(len(misclassification))
-
-    for idx in np.random.choice(misclassification, 5):
-        show_example(idx, X_test, y_test, predictions)
+for idx in np.random.choice(misclassification, 5):
+    show_example(idx, X_test, y_test, predictions)
 
 def show_example(idx, X, y, y_hat):
     """
@@ -128,6 +63,3 @@ def show_example(idx, X, y, y_hat):
         print(f'Prediction is {labels_name[np.argmax(y_hat[idx])]}')
     plt.imshow(cv2.cvtColor(X[idx], cv2.COLOR_BGR2RGB))
     plt.show()
-
-if __name__ == '__main__':
-    main()
